98ebfae25a69d6a26c3f399fab854ae777c87af5
xoxo3 <xxoo3.love@gmail.com>
2024-02-12 01:07:11 +0800

    feat: add form

--- frontend/src/app/airdrop/page.tsx
+++ frontend/src/app/airdrop/page.tsx
@@ -4,5 +4,18 @@ import { useState } from "react";
 
 export default function Home(props: {}) {
   const [reloadAt, setReloadAt] = useState(0);
-  return <PageWrap className="">airdrop</PageWrap>;
+  return (
+    <PageWrap className="">
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLScSiwRac5-46MoiUaz_-3vdaz30u_VxrJUt_yKJndjK8A3h3A/viewform?embedded=true"
+        width="100%"
+        height="1088"
+        frameBorder={0}
+        marginHeight={0}
+        marginWidth={0}
+      >
+        正在加载…
+      </iframe>
+    </PageWrap>
+  );
 }
