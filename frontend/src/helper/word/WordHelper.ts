4067a64d82f504bbef4fcedd806f978af721c021
xoxo3 <xxoo3.love@gmail.com>
2024-02-15 01:28:40 +0800

    feat: add 繁体中文

--- frontend/src/helper/word/WordHelper.ts
+++ frontend/src/helper/word/WordHelper.ts
@@ -1,7 +1,18 @@
+import { LanguageStore } from "@/language/LanguageHelper";
 import { configZH } from "./zh";
+import { configHK } from "./zh-HK";
+import { configEN } from "./en";
 
 export const WordHelper = {
   useInstance() {
-    return configZH;
+    const { currentLanguage } = LanguageStore;
+
+    if (currentLanguage == "zh-HK") {
+      return configHK;
+    }
+    if (currentLanguage == "zh-CN") {
+      return configZH;
+    }
+    return configEN;
   },
 };
