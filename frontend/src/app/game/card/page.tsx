99194de02ded5b2b3b3dd39719c51fb736dfded3
xoxo3 <xxoo3.love@gmail.com>
2024-02-16 17:09:01 +0800

    feat: add bottom tip

--- frontend/src/app/game/card/page.tsx
+++ frontend/src/app/game/card/page.tsx
@@ -2,12 +2,18 @@
 import PageWrap from "@/layout/base/PageWrap";
 import { useState } from "react";
 import { XO3Card } from "./XO3Card";
+import { WordHelper } from "@/language/WordHelper";
 
 export default function Home(props: {}) {
+  const wordSiteInstance = WordHelper.useInstance();
+
   return (
     <PageWrap className="">
-      <div className="flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <XO3Card></XO3Card>
+        <div className="text-sm text-foreground-600">
+          <div className="mt-4">{wordSiteInstance.game.gameTip}</div>
+        </div>
       </div>
     </PageWrap>
   );
